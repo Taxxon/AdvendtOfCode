fa32b450e35eef2c88f6e1db1a954cfbfa5ef4db
Taxxon <te15.emil.kack@umea.nti.se>
2017-12-06 12:14:33 +0100

    Day four done

--- src/day_4.java
+++ src/day_4.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by Emil Käck on 2017-12-03.
@@ -8,26 +9,39 @@ import java.io.IOException;
 public class day_4{
     public static void main(String[] args) {
 
-        int sum = 512;
+        int sum = 0;
+        int line2 = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
             String line;
 
             while ((line = br.readLine()) != null) {
                 String[] word = line.split("\\s");
-                for (int i = 0; i < word.length-1; i++) {
-                    for (int j = (i+1); j < word.length; j++) {
-                        if ((word[i].equals(word[j])) && (i != j)){
-                            sum -= 1;
-                            i = (word.length-1);
+                line2 ++;
+                char[] anagrams1;
+                char[] anagrams2;
 
+                for (int i = 0; i < word.length-1; i++) {
+                    anagrams1 = word[i].toCharArray();
+                    Arrays.sort(anagrams1);
+                    String sortedString = new String(anagrams1);
+                    for (int j = (i+1); j < word.length; j++) {
+                        anagrams2 = word[j].toCharArray();
+                        Arrays.sort(anagrams2);
+                        String sortedString1 = new String(anagrams2);
+                        System.out.println(sortedString+ " " + sortedString1);
+                        if ((sortedString.equals(sortedString1)) && (i != j)){
+                            i = (word.length-1);
+                        } else if ((word.length - 2) == i){
+                            sum++;
                         }
+
                     }
-                }
+                };
             }
         } catch (IOException e){
             System.out.println("NO!");
-        }
+        };
         System.out.println(sum);
     }
 }
