4ae0563d10d07e2e252a54e81dba7c412b211ffd
Taxxon <te15.emil.kack@umea.nti.se>
2017-12-13 07:51:00 +0100

    Day six is done

--- src/day_6.java
+++ src/day_6.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Created by emka15 on 2017-12-06.
+ * Created by Emil Käck on 2017-12-06.
  */
 public class day_6 {
     public static void main(String[] args) {
@@ -12,69 +12,63 @@ public class day_6 {
         ArrayList<String> line = new ArrayList<>();
         ArrayList<Integer> ints = new ArrayList<>();
         String [] words;
-        String look = "";
         int[] input = new int[16];
         String add = "";
         int bigIndex = 0;
         int big = 0;
+        int sum = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
 
             String line1 = br.readLine();
             words = line1.split("\\s", -1);
 
-            for (int i = 0; i < words.length; i++){
+            for (int i = 0; i < words.length; i++) {
                 input[i] = Integer.parseInt(words[i]);
             }
 
 
-            for(int i = 0; i < input.length; i++){
+            for (int i = 0; i < input.length; i++) {
                 ints.add(input[i]);
             }
+            while (true) {
 
-            for (int i = 0; i < ints.size(); i++) {
-                if (i != (ints.size()-1)) {
-                    look += ints.get(i) + " ";
-                } else {
-                    look += ints.get(i);
+                for (int i = 0; i < ints.size(); i++) {
+                    if (ints.get(i) > big) {
+                        bigIndex = i;
+                        big = ints.get(i);
+                    }
                 }
-            }
+                ints.set(bigIndex, 0);
+                int k = 0;
+                int j = bigIndex + 1;
 
-            for (int i = 0; i < ints.size(); i++){
-                if (ints.get(i) > big){
-                    bigIndex = i;
-                    big = ints.get(i);
-                }
-            }
-            System.out.println(bigIndex);
-            System.out.println(ints.get(bigIndex));
-            ints.set(bigIndex, 0);
-            int k = 0;
-            int j = bigIndex+1;
-            while (k != (big+1)){{
-                    ints.set(j, 1 + ints.set(j, (1 + ints.get(j))));
-                    k++;
+                while (k != (big)) {
                     if (j == ints.size()) {
                         j = 0;
                     }
+                    ints.set(j, (ints.get(j) + 1));
+                    k++;
+                    j++;
                 }
-            }
-            System.out.println(ints);
-
 
             for (int i = 0; i < ints.size(); i++) {
-                if (i != (ints.size()-1)) {
-                    add  += ints.get(i) + " ";
-                } else {
-                    add += ints.get(i);
+                    if (i != (ints.size() - 1)) {
+                        add += ints.get(i) + " ";
+                    } else {
+                        add += ints.get(i);
+                    }
                 }
-            }
-            line.add(add);
+                sum++;
 
-            if (line.contains(look)){
-                System.out.println("WORKS!");
+            if (line.contains(add)){
+                System.out.println(sum - line.indexOf(add) - 1);
+                break;
             }
-
+                big = 0;
+                line.add(add);
+                add = "";
+            }
         } catch (IOException e) {
             System.out.println("NO");
         }
---
+++ src/day_7.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Emil Käck on 2017-12-13.
+ */
+public class day_7 {
+    public static void main(String[] args) {
+        
+    }
+}
